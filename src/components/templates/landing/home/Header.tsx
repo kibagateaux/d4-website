c06d7120f2468b65e6d3b89ea5f97c2e5fc9f59e
Isaac Acosta López <isaac.acosta.lop@gmail.com>
2022-01-10 13:56:28 +0000

    Header section

--- src/components/templates/landing/home/Header.tsx
+++ src/components/templates/landing/home/Header.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import Lottie from "react-lottie";
 
 import { Container } from "components/elements";
+import animationData from "lotties/play.json";
 
 const YT_VIDEO = "https://www.youtube.com/embed/ILofy-escS0";
 const VIDEO =
@@ -10,27 +12,66 @@ const isYoutubeVideo = true;
 
 const Header = () => {
   const [showPlayer, setShowPlayer] = useState(false);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   return (
     <section className="bg-theme-base-100 text-theme-base-content pt-20 md:pt-4">
       <Container>
+        {/* Desktop */}
         <div
-          className="cursor-pointer"
-          onClick={() => {
-            setShowPlayer(true);
-          }}
+          className="hidden md:block w-full bg-no-repeat bg-cover bg-center overflow-hidden my-20 max-w-6xl mx-auto"
+          style={{ backgroundImage: "url(/images/token-data-bg.png)" }}
         >
-          <img
-            src="/images/header-desktop.png"
-            alt=""
-            className="hidden md:block w-full max-w-6xl mx-auto py-20"
-          />
-          <img
-            src="/images/header-mobile.png"
-            alt=""
-            className="block md:hidden w-full max-w-md mx-auto py-20"
-          />
+          <img className="w-full" src="/images/header-desktop-1.svg" alt="" />
+          <div className="relative">
+            <img className="w-full" src="/images/header-desktop-2.svg" alt="" />
+            <p className="text-2xl lg:text-3xl xl:text-4xl absolute top-0 w-full mx-auto text-center">
+              We show the Path of the Data
+            </p>
+            <div
+              className="absolute -bottom-4 lg:bottom-0 left-32 lg:left-52 xl:left-64 cursor-pointer flex items-center justify-center"
+              onClick={() => {
+                setShowPlayer(true);
+              }}
+            >
+              <Lottie options={defaultOptions} width={90} />
+              <p className="font-bold font-alt uppercase">Play</p>
+            </div>
+          </div>
+          <img className="w-full" src="/images/header-desktop-3.svg" alt="" />
         </div>
+
+        {/* Mobile */}
+        <div
+          className="block md:hidden w-full bg-no-repeat bg-cover bg-center overflow-hidden my-12 max-w-4xl mx-auto"
+          style={{ backgroundImage: "url(/images/token-data-bg.png)" }}
+        >
+          <img className="w-full" src="/images/header-mobile-1.svg" alt="" />
+          <div className="relative">
+            <img className="w-full" src="/images/header-mobile-2.svg" alt="" />
+            <p className="text-2xl xs:text-3xl sm:text-4xl truncate absolute top-0 pl-4 pb-4 w-full mx-auto text-center">
+              We show the Path of the Data
+            </p>
+            <div
+              className="absolute bottom-0 sm:bottom-4 left-0 sm:left-8 cursor-pointer flex items-center justify-start"
+              onClick={() => {
+                setShowPlayer(true);
+              }}
+            >
+              <Lottie options={defaultOptions} width={80} />
+              <p className="font-bold font-alt uppercase">Play</p>
+            </div>
+          </div>
+          <img className="w-full" src="/images/header-mobile-3.svg" alt="" />
+        </div>
+
       </Container>
 
       {/* video player */}
